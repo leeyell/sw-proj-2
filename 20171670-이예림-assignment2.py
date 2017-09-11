153876765b302759e9b2d5cc6a01e82f043cb0fa
Yerim Lee <lylim5050@kookmin.ac.kr>
2017-09-11 23:47:23 +0900

    콜론 앞 공백 삭제와 코드 설명 주석 추가.

--- 20171670-이예림-assignment2.py
+++ 20171670-이예림-assignment2.py
@@ -1,13 +1,16 @@
+# 콜론 앞의 공백 삭제.
+# 코드 설명 주석 추가.
+
+#while 문 안에서 input 값을 받기 위해 num의 값을 초기화 해준다.
 num=0
 
 while num!=-1:
     num=int(input('Enter a number: '))
-    if num<0 and num!=-1 :
+    #while 문 안에서 num의 값을 받았기 때문에 num == -1 일 때 'retry!'가 출력된 후에 종료되는 것을 방지.
+    if num<0 and num!=-1:
         print('retry!')
-    if num>=0  :
+    if num>=0:
         sum=1
-        for a in range (1,num+1) :
+        for a in range (1,num+1):
             sum=sum*a
         print('%d! = '%num, sum)
-            
-        
